fe78ce2710eb82c768957aedb5d3dc564996318b
aditya <aditya12081998@gmail.com>
2022-12-05 10:40:07 +0530

    v3

--- level2.py
+++ level2.py
@@ -1,7 +1,28 @@
-# hey whats up man
-#hi hwa
-#hipp
+import streamlit as st
+import pandas as pd
+st.set_page_config(page_title='NoCodeAI App',layout='wide')
 
-#adhlasldl
-asdasd
+st.markdown('# NoCodeAI App')
+st.markdown("")
+st.markdown("---")
+
+# st.sidebar.header('Upload your CSV data')
+# uploaded_file = st.sidebar.file_uploader("Upload your file", type=["csv","xlsx"])
+# df=pd.read_csv(uploaded_file)
+
+st.sidebar.header('Upload your CSV data')
+data = st.sidebar.file_uploader('Upload Dataset in .CSV', type=['CSV'])
+df = pd.read_csv(data)
+st.markdown("---")
+
+if st.checkbox('Show  Dataset'):
+    num = st.number_input('No. of Rows', 5, 10)
+    head = st.radio('View from top (head) or bottom (tail)', ('Head', 'Tail'))
+
+    if head == 'Head':
+        st.dataframe(df.head(num))
+    else:
+        st.dataframe(df.tail(num))
+
+st.markdown("---")
 
