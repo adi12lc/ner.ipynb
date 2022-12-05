65c7506a118e38205218c172b690be44e2878e7b
aditya <aditya12081998@gmail.com>
2022-12-06 00:39:53 +0530

    v3

--- level2.py
+++ level2.py
@@ -2,7 +2,9 @@ import streamlit as st
 import pandas as pd
 import warnings
 warnings.filterwarnings("ignore")
+import mysql.connector
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import f1_score,accuracy_score
@@ -13,21 +15,25 @@ st.markdown('# NoCodeAI App')
 st.markdown("")
 st.markdown("---")
 
-data1 = st.radio('View from top (head) or bottom (tail)', ('File', 'Database'))
+data1 = st.sidebar.radio('Choose file input', ('File', 'Database'))
 if data1 == 'File':
     st.sidebar.header('Upload your file')
     dataset = st.sidebar.file_uploader('', type=['CSV', 'xlsx'])
     df = pd.read_csv(dataset)
+elif data1 == 'Database':
+    username = st.sidebar.text_input('Username', '')
+    password = st.sidebar.text_input('Password', '')
+    host = st.sidebar.text_input('Host', '')
+    db = st.sidebar.text_input('Database', '')
+    # table = st.text_input('Database', '')
+    mydb = mysql.connector.connect(user=username, password=password, host=host, database=db)
+    query = st.sidebar.text_input('MySQl query', '')
+    df = pd.read_sql(query, mydb)
+    mydb.close()
 
 
-if st.checkbox('Show  Dataset'):
-    num = st.number_input('No. of Rows', 5, 10)
-    head = st.radio('View from top (head) or bottom (tail)', ('Head', 'Tail'))
-
-    if head == 'Head':
-        st.table(df.head(num))
-    else:
-        st.table(df.tail(num))
+if st.checkbox('Show sample Dataset'):
+    st.table(df.head())
 
     st.markdown("---")
     st.markdown("##### Number of rows and columns in the dataset")
@@ -46,15 +52,17 @@ st.markdown("---")
 
 st.markdown("##### Model Building")
 option = st.sidebar.selectbox('Choose target variable', (df.columns), help="Select dependent variable")
-
-# st.write('Target variable:', option)
+le = LabelEncoder()
+if df[option].dtype == object:
+    df[option] = le.fit_transform(df[option])
 
 Y = df[option]
 X = df.drop(option, axis=1)
 
+
 x_train, xtest, y_train, ytest = train_test_split(X, Y, test_size=0.2, random_state=42)
 
-option = st.sidebar.selectbox('Choose evaluation metric',('Logistic Regression','RandomForestClassifier'))
+option = st.sidebar.selectbox('Choose the algorithm',('Logistic Regression','RandomForestClassifier'))
 
 
 if option == 'Logistic Regression':
@@ -64,7 +72,7 @@ if option == 'Logistic Regression':
 
 elif option == 'RandomForestClassifier':
     st.write("Algorithm:", option)
-    values = st.slider('n_estimators',0, 100, (0))
+    values = st.sidebar.slider('n_estimators',0, 100, (0))
     mod = RandomForestClassifier(n_estimators=values)
     mod.fit(x_train, y_train)
 
